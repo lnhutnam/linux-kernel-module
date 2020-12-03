6e9d0d35786cf6cc1f8fc41e4f6fa10c6f1ffcf2
Brian Doan <brianddoan@gmail.com>
2020-12-03 13:51:42 +0700

    Finish module & add loadable module

--- tmod.c
+++ tmod.c
@@ -12,118 +12,106 @@
 #define MAX_DEV 1
 #define MAX_MSG_LEN 100
 
-static int mychardev_open(struct inode *inode, struct file *file);
-static int mychardev_release(struct inode *inode, struct file *file);
-static long mychardev_ioctl(struct file *file, unsigned int cmd, unsigned long arg);
-static ssize_t mychardev_read(struct file *file, char __user *buf, size_t count, loff_t *offset);
-static ssize_t mychardev_write(struct file *file, const char __user *buf, size_t count, loff_t *offset);
+static int chardev_open(struct inode *inode, struct file *file);
+static int chardev_release(struct inode *inode, struct file *file);
+static long chardev_ioctl(struct file *file, unsigned int cmd, unsigned long arg);
+static ssize_t chardev_read(struct file *file, char __user *buf, size_t len, loff_t *offset);
+static ssize_t chardev_write(struct file *file, const char __user *buf, size_t len, loff_t *offset);
 
-static const struct file_operations mychardev_fops = {
+static const struct file_operations chardev_fops = {
     .owner      = THIS_MODULE,
-    .open       = mychardev_open,
-    .release    = mychardev_release,
-    .unlocked_ioctl = mychardev_ioctl,
-    .read       = mychardev_read,
-    .write       = mychardev_write
+    .open       = chardev_open,
+    .release    = chardev_release,
+    .unlocked_ioctl = chardev_ioctl,
+    .read       = chardev_read,
+    .write       = chardev_write
 };
 
-struct mychar_device_data {
+struct char_device_data {
     struct cdev cdev;
 };
 
 static int dev_major = 0;
-static struct class *mychardev_class = NULL;
-static struct mychar_device_data mychardev_data[MAX_DEV];
+static struct class *chardev_class = NULL;
+static struct char_device_data chardev_data[MAX_DEV];
 
-static int mychardev_uevent(struct device *dev, struct kobj_uevent_env *env) {
+static int chardev_uevent(struct device *dev, struct kobj_uevent_env *env) {
     add_uevent_var(env, "DEVMODE=%#o", 0666);
     return 0;
 }
 
-static int __init mychardev_init(void) {
+static int __init chardev_init(void) {
     int err, i;
     dev_t dev;
 
-    err = alloc_chrdev_region(&dev, 0, MAX_DEV, "mychardev");
-
+    err = alloc_chrdev_region(&dev, 0, MAX_DEV, "chardev");
     dev_major = MAJOR(dev);
-
-    mychardev_class = class_create(THIS_MODULE, "mychardev");
-    mychardev_class->dev_uevent = mychardev_uevent;
+    chardev_class = class_create(THIS_MODULE, "chardev");
+    chardev_class->dev_uevent = chardev_uevent;
 
     for (i = 0; i < MAX_DEV; i++) {
-        cdev_init(&mychardev_data[i].cdev, &mychardev_fops);
-        mychardev_data[i].cdev.owner = THIS_MODULE;
-
-        cdev_add(&mychardev_data[i].cdev, MKDEV(dev_major, i), 1);
-
-        device_create(mychardev_class, NULL, MKDEV(dev_major, i), NULL, "mychardev-%d", i);
+        cdev_init(&chardev_data[i].cdev, &chardev_fops);
+        chardev_data[i].cdev.owner = THIS_MODULE;
+        cdev_add(&chardev_data[i].cdev, MKDEV(dev_major, i), 1);
+        device_create(chardev_class, NULL, MKDEV(dev_major, i), NULL, "chardev%d", i);
     }
-
-    printk("MYCHARDEV INIT\n");
+    printk("CHARDEV INITIALIZED\n");
 
     return 0;
 }
 
-static void __exit mychardev_exit(void) {
+static void __exit chardev_exit(void) {
     int i;
 
     for (i = 0; i < MAX_DEV; i++) {
-        device_destroy(mychardev_class, MKDEV(dev_major, i));
+        device_destroy(chardev_class, MKDEV(dev_major, i));
     }
 
-    class_unregister(mychardev_class);
-    class_destroy(mychardev_class);
-
+    class_unregister(chardev_class);
+    class_destroy(chardev_class);
     unregister_chrdev_region(MKDEV(dev_major, 0), MINORMASK);
-    printk("MYCHARDEV EXIT\n");
+    printk("CHARDEV EXIT\n");
 }
 
-static int mychardev_open(struct inode *inode, struct file *file)
-{
-    printk("MYCHARDEV: Device open\n");
+static int chardev_open(struct inode *inode, struct file *file) {
+    printk("CHARDEV: Device open\n");
     return 0;
 }
 
-static int mychardev_release(struct inode *inode, struct file *file)
-{
-    printk("MYCHARDEV: Device close\n");
+static int chardev_release(struct inode *inode, struct file *file) {
+    printk("CHARDEV: Device close\n");
     return 0;
 }
 
-static long mychardev_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
-{
-    printk("MYCHARDEV: Device ioctl\n");
+static long chardev_ioctl(struct file *file, unsigned int cmd, unsigned long arg) {
+    printk("CHARDEV: Device ioctl\n");
     return 0;
+}
+
+static int power(int num, int times) {
+    int i;
+    int res = num;
+    for (i = 0; i < times - 1; i++) {
+        res *= num; 
+    }
+    return res;
 }
 
 static int gen_randnum(void) {
     int res;
+    uint8_t bound;
     get_random_bytes(&res, sizeof(res));
-
-    return res; 
+    get_random_bytes(&bound, sizeof(bound));
+    return res % power(10, bound % 10);
 }
 
-static uint8_t num_len(int randnum) {
-    uint8_t len = randnum >= 0 ? 0 : 1;
-    do {
-        ++len;
-        randnum /= 10;
-    } while (randnum);
-
-    return len;
-}
-
-static ssize_t mychardev_read(struct file *file, char __user *buf, size_t count, loff_t *offset) {
+static ssize_t chardev_read(struct file *file, char __user *buf, size_t len, loff_t *offset) {
     int randnum = gen_randnum();
-    char data[100];
-    ssize_t bytes = count < (MAX_MSG_LEN-(*offset)) ? count : (MAX_MSG_LEN-(*offset));
-    uint8_t numlen = num_len(randnum);
+    char data[100] = {0};
+    ssize_t bytes = len < (MAX_MSG_LEN-(*offset)) ? len : (MAX_MSG_LEN-(*offset));
     
     sprintf(data, "%d", randnum);
-    data[numlen] = '\0';
-    printk("Generated number: %s, len: %d\n", data, numlen);
-    if (copy_to_user(buf, data, bytes)) {
+    if (copy_to_user(buf, &data, bytes)) {
         return -EFAULT;
     }
 
@@ -131,33 +119,12 @@ static ssize_t mychardev_read(struct file *file, char __user *buf, size_t count,
     return bytes;
 }
 
-static ssize_t mychardev_write(struct file *file, const char __user *buf, size_t count, loff_t *offset) {
-    size_t maxdatalen = 30, ncopied;
-    uint8_t databuf[maxdatalen];
-
-    printk("Writing device: %d\n", MINOR(file->f_path.dentry->d_inode->i_rdev));
-
-    if (count < maxdatalen) {
-        maxdatalen = count;
-    }
-
-    ncopied = copy_from_user(databuf, buf, maxdatalen);
-
-    if (ncopied == 0) {
-        printk("Copied %zd bytes from the user\n", maxdatalen);
-    } else {
-        printk("Could't copy %zd bytes from the user\n", ncopied);
-    }
-
-    databuf[maxdatalen] = 0;
-
-    printk("Data from the user: %s\n", databuf);
-
-    return count;
+static ssize_t chardev_write(struct file *file, const char __user *buf, size_t len, loff_t *offset) {
+    return 0;
 }
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("BrianDoan&LenusNam");
 
-module_init(mychardev_init);
-module_exit(mychardev_exit);
+module_init(chardev_init);
+module_exit(chardev_exit);
